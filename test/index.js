2d8a756ddcbfc87dea349eb91c6efce1c01555b0
lichangwei <lichangwei@love.com>
2012-12-11 11:36:00 +0800

    add licence & initScript attributes for uglify function

--- test/index.js
+++ test/index.js
@@ -2,12 +2,14 @@
 var tools = require('../index.js');
 
 tools.uglifyjs({
-    'input': ['public/scripts/a.js',
+    'input'     : ['public/scripts/a.js',
         'public/scripts/b.js'],
-    'output': 'public/scripts/main.js',
-    'operation': 'uglify' //default
+    'licence'   : 'The MIT Licence. https://github.com/lichangwei/webtools.git',
+    'initScript': '"I am initScript."',
+    'output'    : 'public/scripts/main.js',
+    'operation' : 'uglify' //default
 }).less({
-    'input': ['public/css/style.less'],
-    'output': 'public/css/style.css',
-    'compress': true // default
+    'input'     : ['public/css/style.less'],
+    'output'    : 'public/css/style.css',
+    'compress'  : true // default
 }).manifest( 'public/proj.manifest' );
